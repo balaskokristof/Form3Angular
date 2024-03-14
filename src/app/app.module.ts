4427ff9ba7fd5fd43d263e9c17a6469741fbdc63
Kristof Balasko <kristof@balaskokristof.hu>
2024-03-14 16:54:26 +0100

    kommit!
    ss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { FormComponent } from './form/form.component'; // Importáljuk a FormComponent-et
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
